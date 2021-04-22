1706056c3f430ae7141465eb407c333b464d0a27
FelipeAlonso <xfelipe98@gmail.com>
2021-04-21 23:22:32 -0500

    Se agrego un proyecto de ejemplo, en este caso una tarea del 2017 de numeros complejos

--- prueba/Tarea14.cpp
+++ prueba/Tarea14.cpp
@@ -1,21 +1,21 @@
-//Escuela Colombiana de Ingeniería 
+//Escuela Colombiana de Ingenierï¿½a
 //Asignatura: Algoritmos y programacion de computadores(AYPC-1)
 //Profesora: Ingeniera Patricia Salazar Perdomo
 //Construir un menu en el que el usuario ingrese dos numeros complejos
 //Ademas de tener la opcion de sumarlos, restarlos, multiplicarlos y dividirlos
 //13 de noviembre de 2017
 //Se supone que los datos ingresados son correctos
-//Alumno: Carlos Felipe Alonso Caro
+//Alumno : Carlos Felipe Alonso Caro
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MCC   20  
-#define LC    15   
+#define MCC   20
+#define LC    15
 
 
-struct complejo 
+struct complejo
 {
 	float real;
     float imag;
@@ -33,12 +33,12 @@ void mult_compl(struct complejo, struct complejo, struct complejo *, int);
 void div_compl(struct complejo, struct complejo, struct complejo *, int);
 
 
-    
+
 	do{
         system("cls");
         printf ("\n\n\t*** Operaciones con numeros complejos\n");
-        printf ("\n\t1. Introducir complejos\n");        
-        printf ("\n\t2. Suma complejos\n");                                      
+        printf ("\n\t1. Introducir complejos\n");
+        printf ("\n\t2. Suma complejos\n");
         printf ("\n\t3. Resta complejos\n");
         printf("\n\t4. Multiplica Complejos\n");
         printf("\n\t5. Divide complejos\n");
@@ -58,10 +58,10 @@ void div_compl(struct complejo, struct complejo, struct complejo *, int);
 			   case 4: mult_compl(num1,num2,&mult,most);
 			   		   break;
 			   case 5: div_compl(num1,num2,&div,most);
-			   		   break;       
+			   		   break;
         }
         system ("pause");
-    }while (opc!=6); 
+    }while (opc!=6);
 }
 
 
@@ -106,13 +106,13 @@ void esc_suma(struct complejo n1, struct complejo n2, struct complejo sum)
 {
 printf("\nLa suma de los numeros\n");
 
-	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);	
+	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);
 	else printf("\n\t(%f+%fi) ",(n1).real,(n1).imag);
 
-	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);	
+	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);
 	else printf("y (%f+%fi)",(n2).real,(n2).imag);
 
-	if((sum).imag<0)	    printf(" = (%f%fi)\n\n",(sum).real,(sum).imag);	
+	if((sum).imag<0)	    printf(" = (%f%fi)\n\n",(sum).real,(sum).imag);
 	else printf(" = (%f+%fi)\n\n",(sum).real,(sum).imag);
 }
 
@@ -138,13 +138,13 @@ void esc_resta(struct complejo n1, struct complejo n2, struct complejo sum)
 {
 printf("\nLa resta de los numeros\n");
 
-	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);	
+	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);
 	else printf("\n\t(%f+%fi) ",(n1).real,(n1).imag);
 
-	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);	
+	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);
 	else printf("y (%f+%fi)",(n2).real,(n2).imag);
 
-	if((sum).imag<0)	    printf(" = (%f%fi)\n\n",(sum).real,(sum).imag);	
+	if((sum).imag<0)	    printf(" = (%f%fi)\n\n",(sum).real,(sum).imag);
 	else printf(" = (%f+%fi)\n\n",(sum).real,(sum).imag);
 }
 
@@ -158,26 +158,26 @@ void esc_mult(struct complejo, struct complejo, struct complejo);
 
 if (most==1)
 	{
-	 (*mult).real= (n1).real*(n2).real  -  (n1).imag*(n2).imag;        //a*c-b*d     
+	 (*mult).real= (n1).real*(n2).real  -  (n1).imag*(n2).imag;        //a*c-b*d
 	 (*mult).imag= (n1).real*(n2).imag  +  (n1).imag*(n2).real;        //a*d+b*c
 	 esc_mult(n1, n2, *mult);
 	}
 
-else printf("\n\n No ha ingresado datos\n\n");	
+else printf("\n\n No ha ingresado datos\n\n");
 }
 
 void esc_mult(struct complejo n1, struct complejo n2, struct complejo mult)
 {
 printf("\nLa multiplicacion de los numeros\n");
 
-	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);	
+	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);
 	else printf("\n\t(%f+%fi) ",(n1).real,(n1).imag);
 
-	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);	
+	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);
 	else printf("y (%f+%fi)",(n2).real,(n2).imag);
 
-	if((mult).imag<0)	    printf(" = (%f%fi)\n\n",(mult).real,(mult).imag);	
-	else printf(" = (%f+%fi)\n\n",(mult).real,(mult).imag);	
+	if((mult).imag<0)	    printf(" = (%f%fi)\n\n",(mult).real,(mult).imag);
+	else printf(" = (%f+%fi)\n\n",(mult).real,(mult).imag);
 }
 
 
@@ -186,7 +186,7 @@ printf("\nLa multiplicacion de los numeros\n");
 
 void div_compl(struct complejo n1, struct complejo n2, struct complejo *div, int most)      //divide y escribe el resultado
 {
-void esc_div(struct complejo, struct complejo, struct complejo);	
+void esc_div(struct complejo, struct complejo, struct complejo);
 
 if (most==1)
 	{
@@ -202,13 +202,12 @@ void esc_div(struct complejo n1, struct complejo n2, struct complejo div)
 {
 printf("\nLa division de los numeros\n");
 
-	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);	
+	if((n1).imag<0)	    printf("\n\t(%f%fi) ",(n1).real,(n1).imag);
 	else printf("\n\t(%f+%fi) ",(n1).real,(n1).imag);
 
-	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);	
+	if((n2).imag<0)	    printf("y (%f%fi)",(n2).real,(n2).imag);
 	else printf("y (%f+%fi)",(n2).real,(n2).imag);
 
-	if((div).imag<0)	    printf(" = (%f%fi)\n\n",(div).real,(div).imag);	
-	else printf(" = (%f+%fi)\n\n",(div).real,(div).imag);	
+	if((div).imag<0)	    printf(" = (%f%fi)\n\n",(div).real,(div).imag);
+	else printf(" = (%f+%fi)\n\n",(div).real,(div).imag);
 }
-
